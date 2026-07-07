65b91903921e89126cafb7ac67202f7d813a64a5
monte <williammonte3592@gmail.com>
2026-07-07 14:22:15 +0300

    chore: update devDependencies for linting and TypeScript support
    - Added @eslint/js, eslint, eslint-plugin-vue, and globals for improved linting capabilities.
    - Included typescript-eslint for TypeScript support in linting.

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 const bookStore = {
-    name: 'Flatbooks Technical Books',
+    name: "Flatbooks Technical Books",
     books: [
         {
             id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-            author: 'Marjin Haverbeke',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
+            title: "Eloquent JavaScript: A Modern Introduction to Programming",
+            author: "Marjin Haverbeke",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
         },
         {
             id:2,
-            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-            author: 'Jon Duckett',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
+            title: "JavaScript & JQuery: Interactive Front-End Web Development",
+            author: "Jon Duckett",
+            image: "https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg"
         },
         {
             id:3,
-            title: 'JavaScript: The Good Parts',
-            author: 'Douglas Crockford',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+            title: "JavaScript: The Good Parts",
+            author: "Douglas Crockford",
+            image: "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
         },
         {
             id:4,
-            title: 'JavaScript: The Definitive Guide',
-            author: 'David Flanagan',
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
+            title: "JavaScript: The Definitive Guide",
+            author: "David Flanagan",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
             
         },
         {
             id:5,
-            title: 'You Don\'t Know JS',
-            author: 'Kyle Simpson',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
+            title: "You Don't Know JS",
+            author: "Kyle Simpson",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg"
         },
         {
             id:6,
-            title: 'Cracking the Coding Interview',
-            author: 'Gayle Laakmann McDowell',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
+            title: "Cracking the Coding Interview",
+            author: "Gayle Laakmann McDowell",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg"
             
         }
     ]
@@ -45,10 +44,10 @@ const bookStore = {
 
 // Write your code here!
 
-const bookStoreTitle= document.getElementById('header');
-bookStoreTitle.textcontent = bookstore.name;
+const bookStoreTitle= document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
 
-const booklist= document.getElementById('booklist');
+const booklist= document.getElementById("book-list");
 
 bookStore.books.forEach(book => {
     const bookContainer= document.createElement('li');
@@ -58,11 +57,11 @@ bookStore.books.forEach(book => {
 
     bookTitle.textContent= book.title;
     bookAuthor.textContent= book.author;
-    bookImage.src= book.imageUrl;
+    bookImage.src= book.image;
 
     bookContainer.appendChild(bookTitle);
     bookContainer.appendChild(bookAuthor);
     bookContainer.appendChild(bookImage);
-    bookList.appendChild(bookContainer);
+    booklist.appendChild(bookContainer);
 
 });
